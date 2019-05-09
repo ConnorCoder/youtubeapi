2cb87933a6c64bb56501f312f3977c3842340ba0
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2019-05-09 17:13:50 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,11 +10,11 @@ $(document).ready(function(){
 });
 
 function run() {
-  if (document.getElementById('option') === "sub") {
+  if (document.getElementById('option').value === "sub") {
     var id = document.getElementById('input').value;
     window.location.href = window.location.href + "?sub=" + id;
   }
-  if (document.getElementById('option') === "view") {
+  if (document.getElementById('option').value === "view") {
     var id = document.getElementById('input').value;
     window.location.href = window.location.href + "?view=" + id;
   }
@@ -24,3 +24,16 @@ function run() {
 //$.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpe&5AssTLnUMnaRyUmBGZjKE", function(data) {
 //  var x = JSON.stringify(data.items).split("\"");
 //}) 27 = Sub count, 19 = View count
+
+function live(id) {
+  function update() {
+    $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpe&5AssTLnUMnaRyUmBGZjKE", function(data) {
+      var x = JSON.stringify(data.items).split("\"");
+      if (windowhref.indexOf('?sub') >= 0) {
+        document.getElementById('count').innerHTML = x[27];
+      }else if (windowhref.indexOf('?view') >= 0){
+        document.getElementById('count').innerHTML = x[19];
+      }
+    })
+  }setInterval(update(), 10000)
+}
