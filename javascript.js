9b19267065f5554deaabd748d7b4a67975cd7184
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2019-05-09 13:50:30 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,16 +1,31 @@
 var windowhref = window.location.href;
+var where = ["default"];
 
 $(document).ready(function(){
-  if (windowhref.indexOf("?live") >= 0) {
+  if (windowhref.indexOf("?sub") >= 0) {
+    where[0] = "sub";
+    document.getElementById('live').style.display = "block";
+    document.getElementById('nolive').style.display = "none";
+  }else if (windowhref.indexOf("?view") >= 0) {
+    where[0] = "view";
     document.getElementById('live').style.display = "block";
     document.getElementById('nolive').style.display = "none";
   }
 });
 
 function run() {
-  var id = document.getElementById('input').value;
-$.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpeY5AssTLnUMnaRyUmBGZjKE", function(data) {
-  var x = JSON.stringify(data.items).split("\"");
-  document.getElementById('subscribercount').innerHTML = "Subscriber Count: " + x[27];
-})
+  if (where[0] === "sub") {
+    var id = document.getElementById('input').value;
+    $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpeY5AssTLnUMnaRyUmBGZjKE", function(data) {
+      var x = JSON.stringify(data.items).split("\"");
+      document.getElementById('count').innerHTML = "Subscriber Count: " + x[27];
+    });
+  }
+  if (where[0] === "view") {
+    var id = document.getElementById('input').value;
+    $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpe&5AssTLnUMnaRyUmBGZjKE", function(data) {
+      var x = JSON.stringify(data.items).split("\"");
+      document.getElementById('count').innerHTML = "View Count: " + x[19];
+    })
+  }
 }
