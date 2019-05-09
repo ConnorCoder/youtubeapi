0dbf678cb89b7bb21f839fed577ef42a2a31046f
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2019-05-09 14:11:04 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,31 +1,26 @@
 var windowhref = window.location.href;
-var where = ["default"];
 
 $(document).ready(function(){
-  if (windowhref.indexOf("?sub") >= 0) {
-    where[0] = "sub";
+  if ((windowhref.indexOf("?sub") >= 0) || (windowhref.indexOf("?view") >= 0)) {
     document.getElementById('live').style.display = "block";
     document.getElementById('nolive').style.display = "none";
-  }else if (windowhref.indexOf("?view") >= 0) {
-    where[0] = "view";
-    document.getElementById('live').style.display = "block";
-    document.getElementById('nolive').style.display = "none";
+    var id = windowhref.slice(windowhref.indexOf("="));
+    live(id); //Broadcast for live sub/view count
   }
 });
 
 function run() {
-  if (where[0] === "sub") {
+  if (document.getElementById('option') === "sub") {
     var id = document.getElementById('input').value;
-    $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpeY5AssTLnUMnaRyUmBGZjKE", function(data) {
-      var x = JSON.stringify(data.items).split("\"");
-      document.getElementById('count').innerHTML = "Subscriber Count: " + x[27];
-    });
+    window.location.href = window.location.href + "?sub=" + id;
   }
-  if (where[0] === "view") {
+  if (document.getElementById('option') === "view") {
     var id = document.getElementById('input').value;
-    $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpe&5AssTLnUMnaRyUmBGZjKE", function(data) {
-      var x = JSON.stringify(data.items).split("\"");
-      document.getElementById('count').innerHTML = "View Count: " + x[19];
-    })
+    window.location.href = window.location.href + "?view=" + id;
   }
 }
+
+//Get Information
+//$.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpe&5AssTLnUMnaRyUmBGZjKE", function(data) {
+//  var x = JSON.stringify(data.items).split("\"");
+//}) 27 = Sub count, 19 = View count
