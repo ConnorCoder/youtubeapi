8f43eed17e5aa32a4df3e2f3fef1b71f9951b39d
ConnorCoder <43045469+ConnorCoder@users.noreply.github.com>
2019-05-10 08:47:54 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -55,7 +55,6 @@ function run() {
 function live(id) {
   function update() {
     endInterval(xyz);
-    var xyz = setInterval(update, parseInt(updatespeed[0]));
     $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id + "&key=AIzaSyCSQDB4r8dpeY5AssTLnUMnaRyUmBGZjKE", function(data) {
       var date = new Date();
       if (timestamp.length >= 5) {
@@ -108,7 +107,8 @@ function live(id) {
       if (document.getElementsByClassName('billion')[0].innerHTML != reverse[9]) {$(function() {$('.billion').fadeOut(1000, function() {$(this).text(reverse[9]).fadeIn(1000);});});}
       if (document.getElementsByClassName('tenbillion')[0].innerHTML != reverse[10]) {$(function() {$('.tenbillion').fadeOut(1000, function() {$(this).text(reverse[10]).fadeIn(1000);});});}
       if (document.getElementsByClassName('hundredbillion')[0].innerHTML != reverse[11]) {$(function() {$('.hundredbillion').fadeOut(1000, function() {$(this).text(reverse[10]).fadeIn(1000);});});}
+      var xyz = setInterval(update, parseInt(updatespeed[0]));
     });
   }
-  update();
+  var xyz = setInterval(update, 500);
 }
